c3338ef45fe12fff708527b88194688c400dc808
Andrey Klyuev <48258107+Amur27RUS@users.noreply.github.com>
2019-12-01 23:29:27 +0300

    Update ControllerServlet.java

--- Web_Lab2/src/ControllerServlet.java
+++ Web_Lab2/src/ControllerServlet.java
@@ -14,19 +14,24 @@ public class ControllerServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        //Если истории нет, то мы создаём новую
         if(req.getSession().getAttribute("history")==null){
             req.getSession().setAttribute("history", new History());
         }
 
+        //Принимаем значения X, Y, R
         Double Y = Double.parseDouble(req.getParameter("Y"));
         Double X = Double.parseDouble(req.getParameter("X"));
         Double R = Double.parseDouble(req.getParameter("R"));
 
+        //Проверяем, чтобы значения не были пустыми
         if (X != null && Y != null && R !=null){
 
+            //Перенаправляем на AreaCheckServlet
             RequestDispatcher requestDispatcher = req.getRequestDispatcher("/checkservlet");
             requestDispatcher.forward(req, resp);
 
+            //Если значения пустые, то не перенаправляем, а остаёмся на той же странице
         }else{
             resp.sendRedirect("/Web_Lab2_war_exploded");
         }
